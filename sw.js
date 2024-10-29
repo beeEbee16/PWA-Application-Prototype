e342d95e32d785abf219e8ac0b002417f169ce24
Bradley Blue <b_blue@mail.fhsu.edu>
2024-10-29 10:19:04 -0500

    Added service worker and manifest

---
+++ sw.js
@@ -0,0 +1,47 @@
+const staticCacheName = 'static-v1';
+const dynamicCacheName = 'dynamic-v1';
+const assets = [
+    '/',
+    '/index.html',
+    '/products/gameThrones3.jpg',
+    '/products/harryPotter1.jpg',
+    '/products/lordRings2.jpg',
+    '/products/macgyver1.jpg',
+    '/products/starWars1.jpg',
+    'products/quizList.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css',
+    'https://fonts.googleapis.com/icon?family=Material+Icons'
+];
+
+// install service worker
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(staticCacheName).then(cache => {
+            console.log('caching assets');
+            cache.addAll(assets); 
+        })
+    );
+});
+
+// activate service worker
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(keys.filter(key => key !== staticCacheName).map(key => caches.delete(key)));
+        })
+    );
+});
+
+// service worker fetch
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(cacheRes => {
+            return cacheRes || fetch(event.request).then(fetchRes => {
+                return caches.open(dynamicCacheName).then(cache => {
+                    cache.put(event.request.url, fetchRes.clone());
+                    return fetchRes;
+                })
+            });
+        })
+    );
+});
